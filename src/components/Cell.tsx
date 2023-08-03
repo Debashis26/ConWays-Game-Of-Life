6d522c02c81ab762b0efaca39a4ba7325d09e31b
Debashis26 <deb.office01@gmail.com>
2023-08-03 15:10:46 +0530

    next generation calculation added

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,21 +1,28 @@
-import { Box } from "@mui/material";
 
 type ChildProps = {
   numberProp: number;
+  rowIndex:number;
+  colIndex:number;
+  setBoardValue: (rowIndex: number, colIndex: number, newValue: number) => void;
 };
-const Cell: React.FC<ChildProps> = (numberProp) => {
+const Cell: React.FC<ChildProps> = (props) => {
   // console.log(numberProp.numberProp);
 
   let style = {
     height: "10px",
     width: "10px",
-    backgroundColor: numberProp.numberProp < 1 ? "white" : "black",
+    backgroundColor: props.numberProp < 1 ? "white" : "black",
     margin: "1px",
   };
 
+  let handelClick=()=>{
+    let num:number=props.numberProp === 1 ? 0 : 1;
+    props.setBoardValue(props.rowIndex,props.colIndex,num);
+  }
+  
   return (
     <>
-      <div style={style}></div>
+      <div style={style} onClick={handelClick}></div>
     </>
   );
 };
